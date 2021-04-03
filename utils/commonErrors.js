37e6e4eb68084ef0562236c6272c34d0265cb798
pythonpl <python.polska@gmail.com>
2021-04-03 12:00:56 +0200

    doc update

--- utils/commonErrors.js
+++ utils/commonErrors.js
@@ -1,7 +1,14 @@
 module.exports = {
+    /**
+     * Payment errors definitions
+     */
     PaymentError: 'Something went wrong with your transaction.',
     PaymentDeclined: 'Your card has been declined.',
     PaymentDataIncomplete: 'The data provided is incomplete.',
 
+
+    /**
+     * DB errors definitions
+     */
     TicketDataInvalid: 'The ticketID is invalid.'
 }
