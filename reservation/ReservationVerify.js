2685376b0a2e04c670d79d0b33ece7e1c1d1c666
pythonpl <python.polska@gmail.com>
2021-04-03 14:27:23 +0200

    reservation request verifyer

---
+++ reservation/ReservationVerify.js
@@ -0,0 +1,25 @@
+const validateSchema = require('./ReservationRequestSchema');
+const db = require('../db/dbAPI');
+const ERRORS = require('../utils/commonErrors');
+
+/**
+ * Verifies if the whole request has no syntax errors and doesn't request non-existing tickets/user
+ */
+module.exports = function (data) {
+    return new Promise(async (resolve, reject) => {
+        if (!validateSchema(data)) {
+            return reject(new Error(ERRORS.ReservationRequestInvalid));
+        }
+
+        // Validates tickets and user existence
+        const results = await Promise.all([
+            ...data.ticketID.map(x => db.checkTicketExistence(x)),
+            db.checkUserExistence(data.userID)
+        ]);
+
+        // Resolves true if everything is OK
+        return resolve(results.every(x => x === true));
+    });
+}
+
+
