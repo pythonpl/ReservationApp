172ff78b764c381009cbf40a4241b4f82c2249fc
pythonpl <python.polska@gmail.com>
2021-04-04 10:50:02 +0200

    ReservationClass - comments added

--- db/ReservationClass.js
+++ db/ReservationClass.js
@@ -15,6 +15,9 @@ class Reservation {
     this.paymentStatus = PAYMENT_STATUS.PENDING;
   }
 
+  /**
+   * @returns {Boolean} true if it's not paid successfully yet
+   */
   isWaitingForPayment() {
     return (
       this.paymentStatus === PAYMENT_STATUS.FAILED ||
@@ -22,28 +25,47 @@ class Reservation {
     );
   }
 
+  /**
+   * @returns {Boolean} true if the payment started, but it's not completed yet
+   */
   isLockedForThePayment() {
     return this.paymentStatus === PAYMENT_STATUS.STARTED;
   }
 
+  /**
+   * @returns {Boolean} true if the reservation is already paid
+   */
   isCompleted() {
     return this.paymentStatus === PAYMENT_STATUS.SUCCESSFULL;
   }
 
+  /**
+   * @returns {Boolean} true if the reservation time is up
+   */
   isExpired() {
     return new Date() - this.datetime > PARAMS.RESERVATION_EXPIRY_TIME;
   }
 
+  /**
+   * @returns {Boolean} true if the reservation can be released: it is expired, not paid and not under payment
+   */
   canBeReleased() {
     return (
       !this.isCompleted() && !this.isLockedForThePayment() && this.isExpired()
     );
   }
 
+  /**
+   * This method sets payment status to PAYMENT_STATUS.STARTED
+   */
   beginPayment() {
     this.paymentStatus = PAYMENT_STATUS.STARTED;
   }
 
+  /**
+   * @param {Boolean} status true if the payment ended with success, false otherwise
+   * This method sets payment status to PAYMENT_STATUS.SUCCESSFULL or PAYMENT_STATUS.FAILED
+   */
   endPayment(status) {
     this.paymentStatus = status
       ? PAYMENT_STATUS.SUCCESSFULL
